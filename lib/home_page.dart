972b2f00a45d883a4f86f4381e2894450dc40d37
Curve887 <kurniawanadeputra17@gmail.com>
2024-12-29 23:46:07 +0700

    repair

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-`class HomePage extends StatelessWidget {
+
+class HomePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
